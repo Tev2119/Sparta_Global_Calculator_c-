0a60520b7154f006feceec9d60cb1a557c60b118
tech-w93a <tmuparadzi@spartaglobal.com>
2019-02-14 17:30:37 +0000

    done classes and methods all work expect loop for exit

--- calculator/calculator/Program.cs
+++ calculator/calculator/Program.cs
@@ -8,77 +8,119 @@ namespace calculator
 {
     class Program
     {
+        class AreaOfShapes
+        {
+            public static double Rectangle(double length, double breadth)
+            {
+                Console.WriteLine("Enter length of rectangle : ");
+                length = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter breadth of rectangle : ");
+                breadth = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Area of rectangle: " + (length*breadth));
+                Console.ReadKey();
+                return length * breadth;
+            }
+            public static double Square(double length)
+            {
+                Console.WriteLine("Enter the Length of Side : ");
+                length = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Area of Square: " + (length * length));
+                Console.ReadKey();
+                return length * length;
+
+            }
+            public static double Circle(double radius)
+            {
+                Console.WriteLine("Enter Radius: ");
+                radius = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Your circle has an Area of: " + (Math.PI * radius * radius));
+                Console.ReadKey();
+                return Math.PI * radius * radius;
+            }
+        }
+        class CalculatorParameters
+        {
+            public static double Addition(double num1, double num2)
+            {
+                Console.WriteLine(num1 + " " + "+" + " " + num2 + " = " + (num1 + num2));
+                return num1 + num2;
+            }
+            public static double Subtraction(double num1, double num2)
+            {
+                Console.WriteLine(num1 + " " + "-" + " " + num2 + " = " + (num1 - num2));
+                return num1 - num2;
+            }
+            public static double multiply(double num1, double num2)
+            {
+                Console.WriteLine(num1 + " " + "*" + " " + num2 + " = " + (num1 * num2));
+                return num1 * num2;
+            }
+            public static double Divide(double num1, double num2)
+            {
+                Console.WriteLine(num1 + " " + "/" + " " + num2 + " = " + (num1 / num2));
+                return num1 / num2;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Would you like to calculate (A) or find the area of a shape (B): ");
-            string calc = Console.ReadLine();
-            if (calc == "A")
+            string exit = "exit";
+            do
             {
-                double num1 = 0;
-                double num2 = 0;
-                string opprand;
-                double ans = 0;
-                Console.Write("what is your first number: ");
-                 num1 = Convert.ToDouble(Console.ReadLine());
-                Console.Write("what next number please: ");
-                 num2 = Convert.ToDouble(Console.ReadLine());
-
-                Console.WriteLine("what is your operator sir (/, *, +, -): ");
-                opprand = Console.ReadLine();
-
-                switch (opprand)
+                Console.WriteLine("Would you like to calculate (A) or find the area of a shape (B): ");
+                string calc = Console.ReadLine();
+                if (calc == "A" || calc == "a")
                 {
-                    case "+":
-                        ans = num1 + num2;
-                        break;
-                    case "-":
-                        ans = num1 - num2;
-                        break;
-                    case "/":
-                        ans = num1 / num2;
-                        break;
-                    case "*":
-                        ans = num1 * num2;
-                        break;
+                    Console.Write("what is your first number: ");
+                    double num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("what is the next number please: ");
+                    double num2 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("what is your operator sir (/, *, +, -): ");
+                    string opprand = Console.ReadLine();
+                    switch (opprand)
+                    {
+                        case "+":
+                            CalculatorParameters.Addition(num1, num2);
+                            break;
+                        case "-":
+                            CalculatorParameters.Subtraction(num1, num2);
+                            break;
+                        case "/":
+                           CalculatorParameters.Divide(num1, num2);
+                            break;
+                        case "*":
+                            CalculatorParameters.multiply(num1, num2);
+                            break;
+                    }
+                    //exit = Console.ReadLine();
                 }
-                Console.WriteLine(num1 + "" + opprand + "" + num2 + " = " + ans);
+                else if (calc == "B" || calc == "b")
+                {
+                    string shape;
+                    Console.WriteLine("Which shape Area are you interested; Circle(C), Square(D), Rectangle(E), to exit type(Yes): ");
+                    shape = Console.ReadLine();
+                    if (shape == "C" || shape == "c")
+                    {
+                        AreaOfShapes.Circle(0);
+                    }
+                    else if (shape == "D" || shape == "d")
+                    {
+                        AreaOfShapes.Square(0);
+                    }
+                    else if (shape == "E" || shape == "e")
+                    {
+                        AreaOfShapes.Rectangle(0, 0);
+                    }
+                    else
+                    {
+                        Console.WriteLine("innapropriate operator please retry or if you would like to exit type(Yes)");
+                        shape = Console.ReadLine();
+                    }
+                }
+                Console.ReadKey();
             }
-            else if(calc == "B")
-            {
-                double side, area, radius, length, breadth;
-                string shape;
-                Console.WriteLine("Which shape Area are you interested; Circle(C), Square(D), Triangle(E): ");
-                shape = Console.ReadLine();
-                if (shape == "C")
-                {
-                    Console.WriteLine("Enter Radius: ");
-                    radius = Convert.ToDouble(Console.ReadLine());
-                    area = Math.PI * radius * radius;
-                    Console.WriteLine("Your circle has an Area of: " + area);
-                    Console.ReadKey();
-                }
-                else if(shape == "D")
-                {
-                    Console.WriteLine("Enter the Length of Side : ");
-                    side = Convert.ToDouble(Console.ReadLine());
-                    area = side * side;
-                    Console.WriteLine("Area of Square: " + area);
-                    Console.ReadKey();
-                }
-                else if (shape == "E")
-                {
-                    Console.WriteLine("Enter length of rectangle : ");
-                    length = Convert.ToDouble(Console.ReadLine());
-
-                    Console.WriteLine("Enter breadth of rectangle : ");
-                    breadth = Convert.ToDouble(Console.ReadLine());
-                    area = length * breadth;
-                    Console.WriteLine("Area of rectangle: " + area);
-                    Console.ReadKey();
-                }
-            }
-            Console.ReadKey();
-           
+            while(exit == "Yes");
         }
     }
 }
